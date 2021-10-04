06b118beb382d0ff0e37f81c4b33101f8129a0d7
MaxPasuta <pasutmaks@gmail.com>
2021-10-04 17:46:35 +0700

    api todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import TodoList from "./Todo/TodoList";
+import Context from "./context";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [todos, setTodos] = React.useState([
+        {id: 1, title: 'купить говна', completed: true},
+        {id: 2, title: 'купить каках', completed: false},
+        {id: 3, title: 'купить жижи', completed: false}
+    ])
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/todos/?_limit=90')
+            .then(response => response.json())
+            .then(todos => {
+                setTodos(todos)
+            })
+
+    }, [])
+
+
+
+    function toggleTodo(id) {
+        setTodos(todos.map(todo => {
+                if (todo.id === id) {
+                    todo.completed = !todo.completed
+                }
+                return todo
+            })
+        )
+    }
+
+    function removeTodo(id) {
+        setTodos(todos.filter(todo => todo.id !== id))
+    }
+
+    return (
+        <Context.Provider value={{removeTodo}}>
+            <div className="wrapper">
+                <h1>List</h1>
+                {todos.length ? (
+                    <TodoList todos={todos} onToggle={toggleTodo}/>
+                ) : (
+                    <p>No todos! </p>)}
+            </div>
+        </Context.Provider>
+    );
+
 }
 
 export default App;
